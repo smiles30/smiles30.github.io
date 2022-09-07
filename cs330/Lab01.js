faa95473bffcb0df62c26c8e8370f00a70fee4ae
Sami Miles <samimiles.11037@gmail.com>
2022-09-07 08:20:39 -0400

    Update Lab01.js

--- cs330/Lab01.js
+++ cs330/Lab01.js
@@ -1,1 +1,20 @@
+function labArray(){
 var array = [];
+for (var i = 0; i < 5; i++){
+	var num = Math.floor(100*(Math.random()));
+	array.push(num);
+}
+var mean = 0;
+for (var i = 0; i < array.length; i++){
+	mean += array[i];
+}
+mean /= array.length;
+var greater = [];
+for(var i = 0; i < array.length; i++){
+	if(array[i] > mean){
+		greater.push(array[i]);
+	}
+}
+
+}
+document.querySelector("#arrayOut").innerHTML = "The array is: " + array <br> + "The mean is: " + mean <br> + "Greater: " + greater;
