7fe22638621376f026c93c54fedcd0ef655844b6
Sami Miles <samimiles.11037@gmail.com>
2022-09-29 22:11:53 -0400

    Lab 4 Update

--- cs330/Lab04.js
+++ cs330/Lab04.js
@@ -8,7 +8,9 @@ function init()
     var canvas = document.getElementById( "gl-canvas" );
 
     gl = canvas.getContext('webgl2');
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+if(!gl){
+	alert("WebGL isn't available.");
+}
 
     points=[
     vec2( -0.95 , -0.95 ),
@@ -22,28 +24,27 @@ function init()
     vec2( -0.35 ,  0.45 )
     ];
     
-    //
     //  Configure WebGL
-    //
+
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
     //  Load shaders and initialize attribute buffers
 
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
     // Load the data into the GPU
 
     var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    // Associate out shader variables with our data buffer
+    // Associate shader variables with data buffer
 
-    var positionLoc = gl.getAttribLocation( program, "aPosition" );
-    gl.vertexAttribPointer( positionLoc , 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( positionLoc );
+    var positionLoc = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(positionLoc , 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
 
     render();
 };
@@ -51,13 +52,13 @@ function init()
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
 
-    gl.drawArrays( gl.POINTS, 0, points.length ); //line 1
-    gl.drawArrays( gl.LINES, 0, points.length ); //line 2
-    gl.drawArrays( gl.LINE_STRIP, 0, points.length ); //line 3
-    gl.drawArrays( gl.LINE_LOOP, 0, points.length ); //line 4
-    gl.drawArrays( gl.TRIANGLES, 0, points.length ); //line 5
-    gl.drawArrays( gl.TRIANGLE_STRIP, 0, points.length ); //line 6
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, points.length ); //line 7
+    gl.drawArrays(gl.POINTS, 0, points.length); //line 1
+    gl.drawArrays(gl.LINES, 0, points.length); //line 2
+    gl.drawArrays(gl.LINE_STRIP, 0, points.length); //line 3
+    gl.drawArrays(gl.LINE_LOOP, 0, points.length); //line 4
+    gl.drawArrays(gl.TRIANGLES, 0, points.length); //line 5
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, points.length); //line 6
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, points.length); //line 7
 
 /*
 Tests and expectations/outcomes: 
