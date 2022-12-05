93ad0a34c1cb1d48898741eccf60d08f064792fc
Sami Miles <samimiles.11037@gmail.com>
2022-12-05 09:33:47 -0500

    Update Lab21.js

--- cs330/Lab21.js
+++ cs330/Lab21.js
@@ -111,9 +111,10 @@ function colorPlane() //colorCube
     //quad(0, 1, 5, 4);
     //quad(1, 0, 3, 2);
     //quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
+    //quad(3, 0, 4, 7);
     //quad(4, 5, 6, 7);
     //quad(5, 1, 2, 6);
+quad(1, 0, 2, 3);
 }
 
 function triangle(a, b, c) {
